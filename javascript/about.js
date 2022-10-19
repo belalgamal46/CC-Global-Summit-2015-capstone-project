14431f8b3ed2ac7b6ca23dcd6654e6ab0b0b7cf1
belalgamal46 <belalgamal00@gmail.com>
2022-10-20 01:42:21 +0200

    Add responsive to all screen sizes

--- javascript/about.js
+++ javascript/about.js
@@ -3,9 +3,12 @@ const mobileNavbar = document.querySelector('.mobile-navbar');
 const closeButtonContaienr = document.querySelector('.close-button-contaienr');
 const activePage = window.location.href;
 const navLinks = document.querySelectorAll('.navbar ul li a');
+const previousGlobalSummitsImageContainer = document.querySelector(
+  // eslint-disable-next-line comma-dangle
+  '.previous-global-summits-image-container'
+).children;
 
 const openMobileMenu = () => {
-  console.log('clicked');
   mobileNavbar.style.transform = 'translateX(0)';
 };
 const closeMobileMenu = () => {
@@ -18,6 +21,29 @@ closeButtonContaienr.addEventListener('click', closeMobileMenu);
 navLinks.forEach((link) => {
   if (link.href === activePage) {
     link.classList.add('active');
-    // console.log('tre');
   }
+});
+
+Array.from(previousGlobalSummitsImageContainer).forEach((image) => {
+  image.addEventListener('mouseover', (event) => {
+    if (image.attributes.src.value === event.target.attributes.src.value) {
+      const hoveredImage = `${
+        event.target.attributes.src.value.split('/')[2].split('.')[0]
+      }_hover.png`;
+      image.attributes.src.value = `../images/${hoveredImage}`;
+    }
+  });
+
+  image.addEventListener('mouseleave', (event) => {
+    if (image.attributes.src.value === event.target.attributes.src.value) {
+      const imagePath = event.target.attributes.src.value
+        .split('/')[2]
+        .split('_')[0];
+      const imagePath2 = event.target.attributes.src.value
+        .split('/')[2]
+        .split('_')[1];
+      const joinedImagePath = `../images/${imagePath}_${imagePath2}.png`;
+      image.attributes.src.value = joinedImagePath;
+    }
+  });
 });
